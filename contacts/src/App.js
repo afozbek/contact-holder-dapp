e18d37dccd8762c98cc80d375b8d160b7bc0fec3
Abdullah Furkan Ozbek <abdullah.furkan.ozbek@gmail.com>
2022-02-01 12:19:11 +0300

    removed unnecessary files

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,60 +1,28 @@
-import { useEffect, useState } from 'react';
-import Web3 from 'web3';
-import { CONTRACT_ABI, CONTRACT_ADDRESS } from './config';
+import ContactItem from "components/ContactList/ContactItem";
+import AppContext from "context/AppContext";
+import ContactList from "pages/ContactList";
+import { useEffect, useState } from "react";
+import { Outlet, Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
+import Web3 from "web3";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./config";
 
-function App() {
+var App = () => {
   const [account, setAccount] = useState();
 
-  const [contacts, setContacts] = useState([]);
-  const [contactSmartContractInstance, setContactSmartContractInstance] = useState(null);
+  const [smartContractInstance, setSmartContractInstance] = useState(null);
 
   useEffect(() => {
     async function load() {
 
       const contactList = await getContractInstance();
 
-      setContactSmartContractInstance(contactList)
+      setSmartContractInstance(contactList)
       // set contact list to state variable.
-
-      await getContactList(contactList)
     }
 
     load();
   }, []);
-
-  const getContactList = async (contactSmartContractInstance) => {
-    // Then we get total number of contacts for iteration
-    const counter = await contactSmartContractInstance.methods.count().call();
-    console.log({ counter })
-
-    let totalContacts = []
-    // // iterate through the amount of time of counter
-    for (var i = 1; i <= counter; i++) {
-      // call the contacts method to get that particular contact from smart contract
-      const { id, name, phone } = await contactSmartContractInstance.methods.contactList(i).call();
-      // add recently fetched contact to state variable.
-      totalContacts.push(
-        { id, name, phone }
-      )
-    }
-
-    debugger
-    setContacts(totalContacts);
-  }
-
-
-  // This will send a transaction for creating contact
-  const createContact = async (name, phone) => {
-    const contactList = await getContractInstance();
-
-    return new Promise(async (resolve, reject) => {
-      const tx = await contactList.methods.createContact(name, phone).send({ from: account });
-      debugger
-      tx
-        .on("transactionHash", (hash) => resolve(hash))
-        .on("error", (error) => reject(error))
-    })
-  }
 
   // Returns contactSmartContractInstance
   const getContractInstance = async () => {
@@ -68,19 +36,24 @@ function App() {
   }
 
   return (
-    <div>
-      Your account is: {account}
-      <h1>Contacts</h1>
-      <ul>
-        {
-          Object.keys(contacts).map((contact, index) => (
-            <li key={`${contacts[index].name}-${index}`}>
-              <h4>{contacts[index].name}</h4>
-              <span><b>Phone: </b>{contacts[index].phone}</span>
-            </li>
-          ))
-        }
-      </ul>
+    <div className="app-container">
+      <div className="account">
+        <strong>{account}</strong>
+      </div>
+      <AppContext.Provider value={{
+        account,
+        smartContractInstance,
+        getContractInstance
+      }}>
+        <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem", cursor: "pointer" }}>
+          <Link to="/">Home</Link> {" - "}
+          <Link to="/contacts">Contact List</Link> {" - "}
+        </nav>
+
+        <Outlet />
+      </AppContext.Provider>
+
+
     </div>
   );
 }
--- contacts/src/index.js
+++ contacts/src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "scss/main.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ContactList from "pages/ContactList";
+import ContactItem from "components/ContactList/ContactItem";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/contacts" element={<ContactList />}>
+            {/* <Route path=":contactId" element={<ContactItem />} /> */}
+          </Route>
+
+          <Route path="*"
+            element={
+              <main >
+                <p> nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
